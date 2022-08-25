2b706d67b70d42e189f97d47036ed25cbb90de5f
andrewbastian <andrew.bastian@hey.com>
2022-08-25 16:41:37 -0700

    moved Overlay out of pages folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Background } from "../atoms/Background";
 import { Canvas } from "@react-three/fiber";
-import { Overlay } from "./Overlay";
+import { Overlay } from "../molecules/Overlay";
 
 const Home: NextPage = () => {
   return (
