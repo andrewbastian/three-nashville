4290626feb2721cd593a627243c4f3d51791bea9
andrewbastian <andrew.bastian@hey.com>
2022-08-25 16:35:05 -0700

    bad import from toneal

--- types/types.ts
+++ types/types.ts
@@ -1,43 +1,3 @@
-import { MinorKey, KeyScale, MajorKey } from "@tonaljs/key";
-
-//export interface MajorKeyProps extends MinorKey {
-//  alteration: number;
-//  chordScales: string[];
-//  chords: string[];
-//  chordsHarmonicFunction: string[];
-//  grades: string[];
-//  intervals: string[];
-//  keySignature: string;
-//  minorRelative: string;
-//  scale: string[];
-//  secondaryDominants: string[];
-//  secondaryDominantsMinorRelative: string[];
-//  substituteDominants: string[];
-//  substituteDominantsMinorRelative: string[];
-//  tonic: string;
-//  type: "major";
-//};
-
-//export type KeyProps = {
-//  chordScales: string[];
-//  chords: string[];
-//  chordsHarmonicFunction: string[];
-//  grades: string[];
-//  intervals: string[];
-//  scale: string[];
-//  tonic: string;
-//};
-
-//export interface MinorKeyProps extends MinorKey{
-//  alteration: number;
-//  harmonic: KeyProps;
-//  keySignature: string;
-//  melodic: KeyProps;
-//  natural: KeyProps;
-//  relativeMajor: string;
-//  tonic: string;
-//  type: "minor";
-//};
 export interface CurrCord {
   chord: string;
   functionGroup?: string;
@@ -52,10 +12,6 @@ export type Root = string;
 export type KeyScale = string;
 
 export type ScaleOptions = { [key: string]: "majorKey" | "minorKey" };
-
-type CreateMutable<T> = {
-    -readonly [P in keyof T]: T[P]
-}
 
 export interface Key {
     type: "major" | "minor";
@@ -91,9 +47,3 @@ export interface MiKey extends Key {
     harmonic: keyof Harmonic;
     melodic: keyof Melodic;
 }
-//export type MajKey = CreateMutable<MajorKey>
-
-//export type MiKey = CreateMutable<MinorKey>
-
-//export type KyScale = CreateMutable<MinorKey['natural']>
-
